cfb68bdf79b3828f91d9d7b64cf808d508ed4d9d
Robert Szekeres <rob.szekeres@gmail.com>
2022-10-30 18:25:45 +0100

    Bases Javascript

---
+++ bases/01-const-let.js
@@ -0,0 +1,19 @@
+
+
+
+
+const nombre = 'Robert';
+const apellido = 'Szekeres';
+
+let edad = 27;
+    edad = 28;
+
+
+console.log(nombre, apellido, edad);    // Robert Szekeres 28
+
+if(true){
+    const nombre = 'Maria';
+    console.log(nombre);                // Maria
+}
+
+console.log(edad);                      //28
