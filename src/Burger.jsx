17ad3167c73f53e54b6404bd00c1526cb980e207
ROBERT SZEKERES <87264219+robertszekeres@users.noreply.github.com>
2022-11-21 13:20:52 +0100

    Add files via upload

---
+++ src/Burger.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+
+function Burger() {
+  return (
+    <>
+        <button>
+            <div></div>
+            <div></div>
+            <div></div>
+        </button>
+    </>
+  )
+}
+
+export default Burger
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Burger from "./Burger";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Burger />
+  </React.StrictMode>
+);
